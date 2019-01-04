13bc8ca3b4ea33e58d207d1c3775ec2102d25485
Chanda Mulenga <stconeten@gmail.com>
2019-01-04 02:53:08 +0200

    Removes non-core elements

--- app/config/local/config.php
+++ app/config/local/config.php
@@ -11,10 +11,4 @@ return [
     'environment' => env('APP_ENV'),
     'app_url'     => env('APP_URL'),
     'app_root'    => ROOT_PATH, // dirname(dirname(__FILE__)),
-    'db_driver'   => env('DB_DRIVER'),
-    'db_host'     => env('DB_HOST'),
-    'db_port'     => env('DB_PORT'),
-    'db_database' => env('DB_DATABASE'),
-    'db_username' => env('DB_USERNAME'),
-    'db_password' => env('DB_PASSWORD'),
 ];
--- resources/views/pages/index.php
+++ resources/views/pages/index.php
@@ -7,6 +7,8 @@
         <div class="col-8">
             <h1><?php echo $data['app_name']; ?></h1>
             <p><?php echo "The name of this application is " . $data['app_name'] . '.'; ?></p>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis eius incidunt, nihil, sunt necessitatibus a asperiores reprehenderit excepturi temporibus, accusantium esse? Inventore cupiditate voluptates quibusdam nihil debitis, aliquam magni doloribus provident nam repellat modi incidunt eos odit adipisci voluptate eligendi atque, perspiciatis reiciendis, fugit dicta quia ea? Veritatis consequuntur, accusamus, qui autem deleniti laborum beatae neque dolorum, asperiores porro accusantium amet obcaecati repellendus provident eveniet eius fuga nam eaque non. Soluta ratione at consectetur cumque cupiditate minima ipsum nam impedit dolore, ducimus, laborum delectus possimus suscipit animi sint dolores harum, id obcaecati culpa beatae. Modi dolore atque adipisci tenetur aut?</p>
         </div>
     </div>
 </div>
